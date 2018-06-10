effb287645483e42ed23895fa24f9e3f844eab64
JC Avena <jcavena@gmail.com>
2018-06-10 11:40:28 -0400

    Rearrange route constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  
-  scope :module => 'diaperbanks', :as => "diaperbanks", :path => "" do
-    constraints :subdomain => /^diaperbanks|stagingdiaperbanks$/ do
+  constraints :subdomain => /^diaperbanks|stagingdiaperbanks$/ do
+    scope :module => 'diaperbanks', :as => "diaperbanks", :path => "" do
       get '/' => 'dashboard#show', :as => :root
     end
   end
 
-  root 'public#about'
+  root 'public#home'
   get '/about' => 'public#about'
   resources :partners, only: %i[new create]
 end
